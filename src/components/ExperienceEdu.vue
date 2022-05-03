dd557e360e9f6819d4f4ebebbd4bcb1af5686050
Alexandra <belliraxia@gmail.com>
2022-05-03 14:17:15 -0400

    adds components for resume

---
+++ src/components/ExperienceEdu.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "ExperienceEdu",
+  props: ['compuni', 'jobdeg', 'start', 'end', 'details']
+}
+</script>
+
+<template>
+  <div class="row">
+    <div class="col-md-8"><strong>{{ compuni }} || {{ jobdeg }}</strong></div>
+    <div class="col-6 col-md-4">
+      <p class="float-end">{{ start }} - {{ end }}</p>
+    </div>
+  </div>
+  <li> {{ details }}</li>
+  <br /><br />
+</template>
+
+<style>
+</style>
