a029fa67dc63f6ec8c725f945891c72509d8a3ac
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-05-04 10:22:14 -0700

    adds ResumeView

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ResumeView from '../views/ResumeView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/resume',
+    name: 'resume-view',
+    component: ResumeView
   },
   {
     path: '/about',
