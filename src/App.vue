6e1942583165b1a382a05e90a981c6db86a0ac49
Alexandra <belliraxia@gmail.com>
2022-05-04 14:26:29 -0400

    fixes button and page, fixes console error

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/resume">View Resume</router-link>
+    <router-link to="/resume/1">View Resume</router-link>
   </nav>
   <router-view />
 </template>
